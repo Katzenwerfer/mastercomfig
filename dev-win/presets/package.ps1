3095d179686b81fc1fbc2e64cd111abac802d069
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2025-10-12 00:55:09 -0600

    implement presets package.ps1

---
+++ dev-win/presets/package.ps1
@@ -0,0 +1,36 @@
+# Run script within the directory
+Push-Location -Path $PSScriptRoot
+
+# Delete old VPKs and folders
+Remove-Item -Path '.\*.vpk' -Force
+Get-ChildItem -Path $PWD -Directory | Remove-Item -Recurse -Force
+
+New-Item -Path '.\mastercomfig-base\cfg\presets' -ItemType 'Directory' -Force | Out-Null
+Copy-Item -Path '..\..\config\cfg\presets\*.cfg' -Destination '.\mastercomfig-base\cfg\presets' -Force
+$autoexec_file = '.\mastercomfig-base\cfg\autoexec.cfg'
+@(
+    'exec comfig/define_presets.cfg;'
+    'exec app/pre_init.cfg;exec overrides/pre_init.cfg;'
+    'exec comfig/comfig.cfg;'
+    'exec app/setup_hook.cfg;exec overrides/setup_hook.cfg;'
+    'preset;'
+    'modules_c;'
+    'run_modules;'
+    'exec comfig/echo.cfg;'
+    'exec app/addons.cfg;'
+    'exec overrides/autoexec.cfg;exec app/autoexec.cfg;'
+    'exec comfig/finalize.cfg'
+) | Set-Content -Path $autoexec_file -NoNewline
+
+# Fill folders with common files
+Copy-Item -Path '..\..\config\mastercomfig\*' -Destination '.\mastercomfig-base' -Force -Recurse
+
+. '..\common.ps1'
+
+cleanItems
+
+packageItems
+
+Write-Host
+
+Pop-Location
