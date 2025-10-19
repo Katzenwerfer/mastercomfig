73f6e5b2253bf4fcd028c027aa77695109e4191e
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2025-10-18 20:49:59 -0600

    group together file operations

--- dev-win/comfig/package.ps1
+++ dev-win/comfig/package.ps1
@@ -8,27 +8,19 @@ Get-ChildItem -Path $PWD -Exclude 'package.ps1' | Remove-Item -Force
 Copy-Item -Path '..\..\config\mastercomfig\cfg\comfig\comfig.cfg' -Destination '.\autoexec_template.cfg' -Force
 
 # Remove all lines starting with 'echo'
-(Get-Content -Path '.\autoexec_template.cfg') | Where-Object {
-    -not $PSItem.StartsWith('echo')
-} | Set-Content '.\autoexec_template.cfg'
-
 # Remove all lines starting with 'alias'
-(Get-Content -Path '.\autoexec_template.cfg') | Where-Object {
-    -not $PSItem.StartsWith('alias')
-} | Set-Content '.\autoexec_template.cfg'
-
 # Remove all lines starting with 'block_game_overrides_once'
-(Get-Content -Path '.\autoexec_template.cfg') | Where-Object {
-    -not $PSItem.StartsWith('block_game_overrides_once')
-} | Set-Content -Path '.\autoexec_template.cfg'
-
 # Comment all uncommented cvars
-(Get-Content -Path '.\autoexec_template.cfg') | ForEach-Object {
-    $PSItem -replace '^(\w+.+)', '//$1'
-} | Set-Content -Path '.\autoexec_template.cfg'
-
 # Remove trailing multiple newlines and output with LF line breaks
-(Get-Content -Path '.\autoexec_template.cfg') -join "`n" -replace '\n\n+', "`n`n" -replace '\n\n$', "`n" | Set-Content -Path '.\autoexec_template.cfg' -NoNewline
+((Get-Content -Path '.\autoexec_template.cfg') | Where-Object {
+    -not $PSItem.StartsWith('echo')
+} | Where-Object {
+    -not $PSItem.StartsWith('alias')
+} | Where-Object {
+    -not $PSItem.StartsWith('block_game_overrides_once')
+} | ForEach-Object {
+    $PSItem -replace '^(\w+.+)', '//$1'
+}) -join "`n" -replace '\n\n+', "`n`n" -replace '\n\n$', "`n" | Set-Content -Path '.\autoexec_template.cfg' -NoNewline
 
 # Copy config_template.cfg as autoexec.cfg
 Copy-Item -Path '..\..\config\templates\config\config_template.cfg' -Destination '.\autoexec.cfg' -Force
--- dev-win/common.ps1
+++ dev-win/common.ps1
@@ -1,25 +1,15 @@
 function cleanItems {
     if ($env:zip_package -ne $true) {
         # Remove all comments
+        # Trim leading whitespace
+        # Remove all in-line comments
         Get-ChildItem -Path $PWD -Include '*.cfg', '*.txt', '*.res', '*.nut' -Recurse | ForEach-Object {
             (Get-Content -Path $PSItem) | Where-Object {
                 -not $PSItem.TrimStart().StartsWith('//')
-            } | ForEach-Object {
-                $PSItem -replace '//.*', ''
-            } | Set-Content -Path $PSItem
-        }
-
-        # Trim leading and trailing whitespace
-        Get-ChildItem -Path $PWD -Include '*.cfg', '*.txt', '*.res', '*.nut' -Recurse | ForEach-Object {
-            (Get-Content -Path $PSItem) | ForEach-Object {
-                $PSItem.Trim()
-            } | Set-Content -Path $PSItem
-        }
-
-        # Remove trailing newlines
-        Get-ChildItem -Path $PWD -Include '*.cfg', '*.txt', '*.res', '*.nut' -Recurse | ForEach-Object {
-            (Get-Content -Path $PSItem) | Where-Object {
+            } | Where-Object {
                 $PSItem -match '\S'
+            } | ForEach-Object {
+                $PSItem -replace ' //.*', ''
             } | Set-Content -Path $PSItem
         }
 
@@ -29,30 +19,18 @@ function cleanItems {
         }
 
         # Remove quotes from VDF key values except empty quotes or spaced strings
+        # Normalize spacing between symbols in VDFs
+        # Normalize spacing around double quotes, semicolons, and brackets in VDFs
         Get-ChildItem -Path $PWD -Include 'mtp.cfg', 'dxsupport*.cfg', '*.txt', '*.res' -Exclude 'texture_preload_list.txt' -Recurse | ForEach-Object {
             (Get-Content -Path $PSItem) | ForEach-Object {
-                $PSItem -replace '"([\w-./]+?)"', '$1'
+                $PSItem -replace '"([\w-./]+?)"', '$1' -replace '[\t ]+', ' ' -replace ' ?([";{}]) ?', '$1'
             } | Set-Content -Path $PSItem
         }
 
-        # Normalize spacing between symbols in VDFs
-        Get-ChildItem -Path $PWD -Include 'mtp.cfg', 'dxsupport*.cfg', '*.txt', '*.res' -Exclude 'texture_preload_list.txt' -Recurse | ForEach-Object {
-            (Get-Content -Path $PSItem) -replace '[\t ]+', ' ' | Set-Content -Path $PSItem
-        }
-
-        # Normalize spacing around double quotes, semicolons, and brackets in VDFs
-        Get-ChildItem -Path $PWD -Include 'mtp.cfg', 'dxsupport*.cfg', '*.txt', '*.res' -Exclude 'texture_preload_list.txt' -Recurse | ForEach-Object {
-            (Get-Content -Path $PSItem) -replace ' ?([";{}]) ?', '$1' | Set-Content -Path $PSItem
-        }
-
         # Convert CRLF to LF line breaks in all files
-        Get-ChildItem -Path $PWD -Include '*.cfg', '*.txt', '*.res', '*.nut' -Recurse | ForEach-Object {
-            (Get-Content -Path $PSItem -Raw) -replace '\r', '' | Set-Content -Path $PSItem -NoNewline
-        }
-
         # Remove trailing multiple final newlines in all files
         Get-ChildItem -Path $PWD -Include '*.cfg', '*.txt', '*.res', '*.nut' -Recurse | ForEach-Object {
-            (Get-Content -Path $PSItem -Raw) -replace '\n\n+$', "`n" | Set-Content -Path $PSItem -NoNewline
+            (Get-Content -Path $PSItem -Raw) -replace '\r', '' -replace '\n\n+$', "`n" | Set-Content -Path $PSItem -NoNewline
         }
     }
 }
